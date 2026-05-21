b9756c133acd896933cc55c7ccb2c468a8ae9c26
Igor Dimchevski <i.dimcevski@gmail.com>
2026-05-21 21:15:14 +0200

    add ralph once

---
+++ ralph-once.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+codex "@.scratch\remove-identity-passthrough-tests\PRD.md \
+1. Read the PRD. \
+2. Find the next incomplete issue and implement it. \
+3. Commit your changes. \
+4. Update the issue file status when you're done. \
+ONLY DO ONE TASK AT A TIME."
